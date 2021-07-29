bc3c5b29e21491687c71471706f3b0bb50aad99d
tomcel <tcel@protonmail.com>
2021-07-29 12:18:26 +0200

    Added SQLite database with example data and changed db path

--- ShopRestAPI/Models/ShopContext.cs
+++ ShopRestAPI/Models/ShopContext.cs
@@ -10,15 +10,11 @@ namespace ShopRestAPI.Models
         public DbSet<Category> Categories { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<ProductImage> ProductsImages { get; set; }
-     
-        public string DbPath { get; private set; }
+
+        public string DbPath { get; private set; } = "database.db";
 
         public ShopContext()
         {
-            var folder = Environment.SpecialFolder.LocalApplicationData;
-            var path = Environment.GetFolderPath(folder);
-            DbPath = $"{path}{System.IO.Path.DirectorySeparatorChar}database.db";
-
             Database.EnsureCreated();
         }
 
